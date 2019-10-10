4f0a1c48901b6bc35a1f187745288ff4a581fb04
ilabstaff <ilabstaff@172.16.116.77>
2019-10-09 19:22:56 -0700

    ghjsfajhdisaf

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -9,9 +9,10 @@ public class Houses {
 	static Robot rob =new Robot();
 	
 	public static void drawhouse(String tall, Color col) {
+		
 		int height = 0;
 		if (tall.equals("small")) {
-			height = 80;
+			height = 50;
 		}
 		else  if (tall.equals("medium")) {
 			height = 140;
@@ -20,10 +21,15 @@ public class Houses {
 			height = 270;
 		}
 		rob.setPenColor(col);
+		
 		rob.move(height);
-		rob.turn(90);
-		rob.move(40);
-		rob.turn(90);
+		
+		if (height <= 140) {
+			pointedroof();
+		}
+		else {
+			flatroof();
+		}
 		rob.move(height);
 		rob.setPenColor(Color.green);
 		rob.turn(-90);
@@ -33,7 +39,22 @@ public class Houses {
 		
 		
 	}
+	
+	public static void pointedroof() {
+		rob.turn(45);
+		rob.move(30);
+		rob.turn(90);
+		rob.move(30);
+		rob.turn(45);
 		
+		
+	}
+	public static void flatroof() {
+		rob.turn(90);
+		rob.move(30);
+		rob.turn(90);
+		
+	}
 	public static void main(String[] args) {
 		rob.setSpeed(50);
 		rob.setX(20);
